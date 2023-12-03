e0fd40332a8bf1eb55be2152149f019df34896a3
sachinsingh <sachinseengh@gmail.com>
2023-12-03 11:23:21 +0545

    Python Training

---
+++ Day-2/Hello.py
@@ -0,0 +1,16 @@
+print("Hello world")
+print('Hello World',"hello world",sep='')
+print("sachin",end='')
+print("singh")
+
+
+print(23,"sachin")
+
+a=2
+# print(a)
+b=10
+print(f"The first number is {a},Second number is {b}")
+print("The sum is ",a+b)
+print(f"The sum is {a+b}")
+print("Expo",a**a)
+print(type(a))
