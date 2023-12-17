f432a2a9ca6c2a69e3f93676a2d8c53d9f21baa5
sachinsingh <sachinseengh@gmail.com>
2023-12-17 11:48:13 +0545

    frozen set

--- Day-7/assaignment.py
+++ Day-7/assaignment.py
@@ -1,6 +1,6 @@
 
 fruits={"apple","banana","grapes"}
-vegetable={"patato","tamato","lauki"}
+vegetable={"patato","tamato","lauki","grapes"}
 
 
 similar =fruits.union(vegetable);
@@ -8,3 +8,17 @@ print(similar)
 
 all = fruits.intersection(vegetable)
 print(all)
+
+
+numbers = len(fruits)
+print(numbers)
+
+for  i in fruits:
+    print(i)
+    
+    
+# Immutable set are called frozen set
+
+number=frozenset({"1","2"})
+# number[0]="sachin"
+print(number)
--- Day-7/set.py
+++ Day-7/set.py
@@ -1,6 +1,7 @@
 # Set 
 # Duplicate not allow
 # set are unordered
+# Mutable
 
 a={1,2,3,4,5,6}
 b={5,6,7,8,9,10}
