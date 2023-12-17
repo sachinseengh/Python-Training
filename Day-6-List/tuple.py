c2e2bfc2d52f41bda00bacbfe9f1c0665e060dc7
sachinsingh <sachinseengh@gmail.com>
2023-12-17 11:53:32 +0545

    tuple in python

--- Day-6-List/tuple.py
+++ Day-6-List/tuple.py
@@ -6,6 +6,15 @@ print(num_2.index(75))
 
 num_1=[4,5,7,85,75]
 
+# tuple cannot be changed or manipulated
+# manipulation methods are unavailable
+# num_2[0]=8
+
+
 
 print("list size :",sys.getsizeof(num_1))
 print("tuple size :",sys.getsizeof(num_2))
+
+for i in num_2:
+    print(i)
+    
