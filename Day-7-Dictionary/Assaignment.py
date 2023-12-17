2b6e2554c743e2fa3e3c0764eb27a42a525901f6
sachinsingh <sachinseengh@gmail.com>
2023-12-17 12:14:10 +0545

    Dictionary assaignment

---
+++ Day-7-Dictionary/Assaignment.py
@@ -0,0 +1,17 @@
+
+# it can be used to store date of birth of friends
+# can automate when it is birthday date from my fb automatically 
+# get posted a birthday wishes in their fb timeline
+
+phoneBook={
+    "Surya":9815487554,
+    "santosh":84455466,
+    "sachin":42541857,
+    "sandy":546745617
+}
+
+
+print(phoneBook.get("Surya"))
+print(phoneBook.get("santosh"))
+print(phoneBook.get("sandy"))
+print(phoneBook.get("sachin"))
