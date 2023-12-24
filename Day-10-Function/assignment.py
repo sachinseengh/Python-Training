7e6805be907e379c6a98f4a1af4f35a66b1114ce
sachinsingh <sachinseengh@gmail.com>
2023-12-24 11:45:16 +0545

    Function in python

---
+++ Day-10-Function/assignment.py
@@ -0,0 +1,14 @@
+def calculate(a,b):
+    print(f"The sum is {a+b} ")
+    print(f"The mul is {a*b} ")
+    print(f"The div is {a/b} ")
+    print(f"The sub is {a-b} ")
+
+
+
+
+a= int(input("Enter your first number : "))
+
+b= int(input("Enter your second number : "))
+
+calculate(a,b)
---
+++ Day-10-Function/function.py
@@ -0,0 +1,17 @@
+def name_function(name):
+    print("your name is " + name)
+    
+    # return True
+    return True
+    
+    
+    
+res=name_function("sachin")
+print(res)
+
+
+    
+
+
+
+
