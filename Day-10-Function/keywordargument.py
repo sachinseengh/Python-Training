c82589382230011b64fbb0b967ccfbbdff860a48
sachinsingh <sachinseengh@gmail.com>
2023-12-25 11:26:28 +0545

    keyword arrrgument

---
+++ Day-10-Function/keywordargument.py
@@ -0,0 +1,20 @@
+
+# Keyword argument
+
+def person(name,address,age):
+    
+    print("Name :" ,name)
+    print("Address :",address)
+    print("Age : ",age)
+
+# person("kathmandu","Ram",23) positional argument
+
+
+
+person(name="sachin",address="kathmandu",age=51) #keyword argument
+print("=================================================================")
+
+# we can also do this
+person("sachin",address="kathmandu",age=555)
+
+print("=================================================================")
