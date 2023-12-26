89eb110bc247ef8d635698580dc4121c16095fbe
sachinsingh <sachinseengh@gmail.com>
2023-12-26 12:09:46 +0545

    calculator in class and object

---
+++ Day-12-class/calculator.py
@@ -0,0 +1,23 @@
+class Calculator:
+    def __init__(self,n1,n2):
+        self.n1= n1
+        self.n2= n2
+        
+    def sum(self):
+        print(f"The sum is {self.n1+self.n2}")
+        
+    def sub(self):
+        print(f"The sub is {self.n1-self.n2}")
+        
+    def div(self):
+        print(f"The div is {self.n1/self.n2}")
+        
+    def mul(self):
+        print(f"The mul is {self.n1*self.n2}")
+        
+    
+c1= Calculator(5,5)
+c1.sum()
+c1.sub()
+c1.div()
+c1.mul()
